fff6435d32284804ac47e8fcbf41c1ece03b7bdc
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2021-04-20 11:35:30 -0300

    added skeleton text to 03-componentsApp

--- 03-components/src/app/services/data.service.ts
+++ 03-components/src/app/services/data.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Injectable } from '@angular/core';
+import { delay } from 'rxjs/operators'
+
+// Interfaces
 import { AlbumsData } from '../interfaces/albumsData.interface';
 import { UsersData } from '../interfaces/usersData.interface';
 import { HeroesData } from '../interfaces/heroesData.interface';
@@ -16,15 +18,18 @@ export class DataService {
 
   getUsersData() {
 
-    return this.http.get<UsersData[]>('https://jsonplaceholder.typicode.com/users')
+    return this.http.get<UsersData[]>('https://jsonplaceholder.typicode.com/users');
       
   }
 
   getAlbums() {
-    return this.http.get<AlbumsData[]>('https://jsonplaceholder.typicode.com/albums')
+    return this.http.get<AlbumsData[]>('https://jsonplaceholder.typicode.com/albums');
   }
 
   getHeroes() {
     return this.http.get<HeroesData[]>('/assets/data/superheroes.json')
+      .pipe(
+        delay(5000)
+      )
   }
 }
